3e740a495b48e06f0b39417db5946c0c3ea5cab8
Imam Teguh <imam.teguh33@gmail.com>
2025-11-07 17:41:42 +0700

    fix: perbaiki zod version

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
 export const PatientFormSchema = z.object({
-  first_name: z.string()
+  first_name: z
+    .string()
     .trim()
     .min(2, "First name must be at least 2 characters")
     .max(30, "First name can't be more than 50 characters"),
@@ -13,18 +14,21 @@ export const PatientFormSchema = z.object({
   date_of_birth: z.coerce.date(),
   gender: z.enum(["MALE", "FEMALE"], { message: "Gender is required" }),
   phone: z.string().min(10, "Enter phone number").max(10, "Enter phone number"),
-  email: z.email("Invalid email address."),
-  address: z.string()
+  email: z.string().email("Invalid email address."),
+  address: z
+    .string()
     .min(5, "Address must be at least 5 characters")
     .max(500, "Address must be at most 500 characters"),
   marital_status: z.enum(
     ["married", "single", "divorced", "widowed", "separated"],
     { message: "Marital status is required." }
   ),
-  emergency_contact_name: z.string()
+  emergency_contact_name: z
+    .string()
     .min(2, "Emergency contact name is required.")
     .max(50, "Emergency contact must be at most 50 characters"),
-  emergency_contact_number: z.string()
+  emergency_contact_number: z
+    .string()
     .min(10, "Enter phone number")
     .max(10, "Enter phone number"),
   relation: z.enum(["mother", "father", "husband", "wife", "other"], {
@@ -36,17 +40,20 @@ export const PatientFormSchema = z.object({
   medical_history: z.string().optional(),
   insurance_provider: z.string().optional(),
   insurance_number: z.string().optional(),
-  privacy_consent: z.boolean()
+  privacy_consent: z
+    .boolean()
     .default(false)
     .refine((val) => val === true, {
       message: "You must agree to the privacy policy.",
     }),
-  service_consent: z.boolean()
+  service_consent: z
+    .boolean()
     .default(false)
     .refine((val) => val === true, {
       message: "You must agree to the terms of service.",
     }),
-  medical_consent: z.boolean()
+  medical_consent: z
+    .boolean()
     .default(false)
     .refine((val) => val === true, {
       message: "You must agree to the medical treatment terms.",
@@ -133,7 +140,7 @@ export const VitalSignsSchema = z.object({
   body_temperature: z.coerce.number({
     message: "Enter recorded body temperature",
   }),
-  heart_rate: z.string({ message: "Enter recorded heartbeat rate" }),
+  heartRate: z.string({ message: "Enter recorded heartbeat rate" }),
   systolic: z.coerce.number({
     message: "Enter recorded systolic blood pressure",
   }),
